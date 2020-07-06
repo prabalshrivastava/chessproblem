db3463931df9e3b3ca54714363b4540a08ffebf4
Prabal Shrivastava <>
2020-07-06 16:51:50 +0530

    initial commit

---
+++ src/main/java/com/technogise/chessproblem/ChessproblemApplication.java
@@ -0,0 +1,13 @@
+package com.technogise.chessproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessproblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessproblemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/technogise/chessproblem/ChessproblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technogise.chessproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessproblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
